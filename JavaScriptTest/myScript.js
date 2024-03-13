92e077676d426c20d304a1370d1db0a58fac3157
Kevin1j <johnsonrkevin@gmail.com>
2024-03-13 16:42:21 -0400

    more test
    test

---
+++ JavaScriptTest/myScript.js
@@ -0,0 +1,7 @@
+function myFunction() {
+	let a, b, c;  // Declare 3 variables
+	a = 5;        // Assign the value 5 to a
+	b = 6;        // Assign the value 6 to b
+	c = a + b;
+	document.getElementID("demo1").innerHTML = c;
+}
