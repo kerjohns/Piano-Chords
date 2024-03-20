ef831dce6a7ae0af5d352ee5c28fcaf08d61feb4
ajvangor <ajvangor@mtu.edu>
2024-03-20 13:10:58 -0400

    linked

---
+++ chords.js
@@ -0,0 +1,20 @@
+function major(root) {
+	let third, fifth;
+	third = (root + 4) % 12;
+	fifth = (root + 7) % 12;
+	return [numToNote(root), numToNote(third), numToNote(fifth)];
+}
+
+function minor(root) {
+	let third, fifth;
+	third = (root + 4) % 12;
+	fifth = (root + 7) % 12;
+	return [numToNote(root), numToNote(third), numToNote(fifth)];
+}
+
+
+//function that will convert a number to its respective note
+function numToNote(input) {
+    const noteNames = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+    return noteNames[(input - 1 + 12) % 12];
+}
