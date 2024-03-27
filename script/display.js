eae84def27ab74b195a9ae40cd363ce08256dd6b
ajvangor <ajvangor@mtu.edu>
2024-03-27 13:24:07 -0400

    Update display.js

--- script/display.js
+++ script/display.js
@@ -1,9 +1,34 @@
-// Attach event listener to the button for displaying chord notes
+//add the event listeners to the buttons so that it all works.
 document.addEventListener("DOMContentLoaded", function() {
     var flip = document.getElementById('flip');
-    var next = document.getElementById('next');
     var chordNotes; // Declare chordNotes outside the event listeners
+    var isChordDisplayed = false; // Initialize to false since the chord name is shown first
 
+    function displayChordNotes() {
+        // Display chord notes
+        noteName.style.display = 'none';
+        chordName.style.display = 'block';
+
+        //display them from the corresponding elements
+        if (chordNotes) {
+            document.getElementById("chordName").innerHTML = chordNotes[0] + "    " + chordNotes[1] + "    " + chordNotes[2];
+        }
+        isChordDisplayed = true; //chords are now displayed, set true
+    }
+
+    flip.addEventListener('click', function(event) {
+        if (isChordDisplayed) {
+            // hide chord notes if the chord is displayed
+            noteName.style.display = 'block';
+            chordName.style.display = 'none';
+            isChordDisplayed = false;
+        } else {
+            //if it is not display the chord notes
+            displayChordNotes();
+        }
+    });
+
+    var next = document.getElementById('next'); //next function
     next.addEventListener('click', function(event) {
         noteName.style.display = 'block';
         chordName.style.display = 'none';
@@ -23,22 +48,9 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("noteName").innerHTML = numToNote(randomRoot) + " minor";
         }
 
-        console.log(chordNotes);
-    })
+        // Reset isChordDisplayed to true since chord is displayed after next click
+        isChordDisplayed = false;
 
-    flip.addEventListener('click', function(event) {
         console.log(chordNotes);
-        noteName.style.display = 'none';
-        chordName.style.display = 'block';
-
-        // Display chord notes in the corresponding <p> elements
-        if (chordNotes) {
-            // Display chord notes in the corresponding <p> elements
-            document.getElementById("chordName").innerHTML = chordNotes[0] + "    " + chordNotes[1] + "    " + chordNotes[2];
-        }
-        /*
-        document.getElementsByClassName("notes")[1].innerHTML = chordNotes[1];
-        document.getElementsByClassName("notes")[2].innerHTML = chordNotes[2];
-        */
     });
 });
